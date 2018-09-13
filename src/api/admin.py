cc582503f2008e0901ce492de3605670c2a3ccaa
Allen Anthes <abanthes@gmail.com>
2018-09-13 16:07:34 -0500

    inline User extension model UserInfo in admin interface

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import Channel, UserInfo
+
+
+class UserInfoInline(admin.StackedInline):
+    model = UserInfo
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserInfoInline,)
+
 
 admin.site.register(Channel)
 admin.site.register(UserInfo)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
